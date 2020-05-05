956c2ceb3c169d765aacd4b9042fc34aca8bd7a9
Mihai Dascalescu <dascalescumihaipetru@gmail.com>
2020-05-05 17:11:11 +0200

    Factory and RecycleItem, Collision is not working

---
+++ Assets/Scripts/Factory.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory : MonoBehaviour
+{
+	[SerializeField]
+	private int factoryType;
+	private RecycleItem itemToBeRecycled;
+	
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (itemToBeRecycled != null && CompareWithReceivedItem(itemToBeRecycled))
+		{
+			
+			Destroy(itemToBeRecycled.gameObject);
+			Debug.Log("Distrus");
+		}
+    }
+	bool CompareWithReceivedItem(RecycleItem recycleItem)
+	{
+		if(recycleItem.itemType == factoryType)
+		{
+			Debug.Log("adevarat");
+			return true;
+			
+		}
+		else
+		{
+			return false;
+		}
+	}
+	private void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log("Collided");
+		if (collision.gameObject.tag == "recycleItem")
+		{
+			itemToBeRecycled = collision.gameObject.GetComponent<RecycleItem>();
+			Debug.Log("Collided");
+		}
+	}
+	
+}
---
+++ Assets/Scripts/RecycleItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecycleItem : MonoBehaviour
+{
+	[SerializeField]
+	public int itemType { get; set; }
+	public int potentialItemType { get; private set; }
+    void Start()
+    {
+		
+		potentialItemType = Random.Range(0, 2);
+		itemType = potentialItemType;
+		Debug.Log(itemType);
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (itemType == 0)
+		{
+			gameObject.GetComponent<Renderer>().material.SetColor("_BaseColor", Color.green);
+		}
+		if (itemType == 1)
+		{
+			gameObject.GetComponent<Renderer>().material.SetColor("_BaseColor", Color.blue);
+		}
+		if (itemType == 2)
+		{
+			gameObject.GetComponent<Renderer>().material.SetColor("_BaseColor", Color.red);
+		}
+	}
+}
