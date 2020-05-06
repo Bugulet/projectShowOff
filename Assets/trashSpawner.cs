2ca2b08a37c08578dd4c14f930e6871cd1d6ccb2
Sebi <eusebiu.buga@gmail.com>
2020-05-06 14:28:09 +0200

    Added trash spawner

---
+++ Assets/trashSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trashSpawner : MonoBehaviour
+{
+    GameObject trashItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trashItem = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!trashItem.GetComponent<Lean.Touch.LeanSelectable>().IsSelected)
+        {
+            trashItem.transform.localPosition = new Vector3(0, 0, 0);
+        }
+    }
+}
