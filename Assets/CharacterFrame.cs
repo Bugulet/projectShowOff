1c1f6f568bb8d830e477ba49ee54b3d8d7b5be76
Sebi <eusebiu.buga@gmail.com>
2020-06-11 03:40:39 +0200

    Added start scene, material instancing, end screen borders

---
+++ Assets/CharacterFrame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterFrame : MonoBehaviour
+{
+    [SerializeField]
+    private Transform frame;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    public void SetFrameToCharacter()
+    {
+        frame.gameObject.SetActive(true);
+        frame.transform.position = new Vector2(transform.position.x, transform.position.y);
+    }
+}
