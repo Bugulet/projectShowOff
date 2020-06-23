ca34ad118682b2b51b7efc83955bf2a55f633bb7
Sebi <eusebiu.buga@gmail.com>
2020-06-24 01:28:29 +0200

    CSV and feedback panel

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using System;
+using System.IO;
+
+
 public class Scoreboard : MonoBehaviour
 {
     private AnimalTeam currentSessionTeam = AnimalTeam.Empty;
 
     private string currentSessionName = "";
+
+    private int currentFeedback = 5;
 
     [SerializeField]
     private GameObject scoreGameObject, currentSessionGameObject, scoreElement;
@@ -15,10 +21,11 @@ public class Scoreboard : MonoBehaviour
 
     public void Start()
     {
+        PrintAllSessions();
+
         if (scoreGameObject != null && currentSessionGameObject!=null)
         {
             
-
             List<ScoreObject> scoreSessions = GetAllSessions();
 
             ScoreObject currentSession = scoreSessions[scoreSessions.Count - 1];
@@ -61,7 +68,9 @@ public class Scoreboard : MonoBehaviour
 
         string name = currentSessionName;
 
-        ScoreObject sessionObject = new ScoreObject(score, name, team);
+        int feedback = currentFeedback;
+
+        ScoreObject sessionObject = new ScoreObject(score, name, team,feedback);
 
         string sessionString = JsonUtility.ToJson(sessionObject);
 
@@ -78,7 +87,33 @@ public class Scoreboard : MonoBehaviour
         PlayerPrefs.SetString(sessionCounter + "sessionKey", sessionString);
         PlayerPrefs.SetInt("sessionCounter", sessionCounter + 1);
 
-        print(sessionString);
+        //create entry on text file
+        {
+            sessionString += string.Format(" Played: {0} seconds, at {1}", Time.time, System.DateTime.Today);
+
+            print(sessionString);
+
+            string path = @"museumFile.txt";
+            // This text is added only once to the file.
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(sessionString);
+                }
+                return;
+            }
+
+            // This text is always added, making the file longer over time
+            // if it is not deleted.
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(sessionString);
+            }
+        }
+        
+        
     }
 
     public void PrintAllSessions()
@@ -118,13 +153,15 @@ public class Scoreboard : MonoBehaviour
     {
         int sessionCounter;
 
-        int score = Random.Range(0,150);
+        int score = UnityEngine.Random.Range(0,150);
 
-        AnimalTeam team = (AnimalTeam) Random.Range(1,4);
+        AnimalTeam team = (AnimalTeam)UnityEngine.Random.Range(1,4);
 
         string name = "Random "+Time.frameCount%1000;
 
-        ScoreObject sessionObject = new ScoreObject(score, name, team);
+        int feedback = UnityEngine.Random.Range(1, 6);
+
+        ScoreObject sessionObject = new ScoreObject(score, name, team,feedback);
 
         string sessionString = JsonUtility.ToJson(sessionObject);
 
@@ -142,7 +179,12 @@ public class Scoreboard : MonoBehaviour
         PlayerPrefs.SetInt("sessionCounter", sessionCounter + 1);
         print("Added random session: "+ sessionString);
     }
+
+    //getters
+    public int GetFeedback() { return currentFeedback; }
+
     //setters
+    public void SetFeedback(int value) { currentFeedback = value; }
     public void SetFoxTeam() { currentSessionTeam = AnimalTeam.Fox; }
     public void SetDogTeam() { currentSessionTeam = AnimalTeam.Dog; }
     public void SetCatTeam() { currentSessionTeam = AnimalTeam.Cat; }
