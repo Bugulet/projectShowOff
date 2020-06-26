070adbc4989c343f27506bc2c0edd01bc447d986
Sebi <eusebiu.buga@gmail.com>
2020-06-26 04:41:54 +0200

    HexagonGrid + Tool added (there is also a prefab )

---
+++ Assets/HexagonGrid.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexagonGrid : MonoBehaviour
+{
+    [SerializeField]
+    private Material[] materials=new Material[4];
+    private int currentMaterial = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PreviousMaterial()
+    {
+        currentMaterial--;
+        if (currentMaterial < 0)
+            currentMaterial = materials.Length - 1;
+        changeMaterial();
+    }
+
+    public void NextMaterial()
+    {
+        
+        currentMaterial++;
+        if (currentMaterial > materials.Length-1)
+            currentMaterial = 0;
+        changeMaterial();
+    }
+
+    private void changeMaterial()
+    {
+        GetComponent<Renderer>().material = materials[currentMaterial];
+    }
+}
